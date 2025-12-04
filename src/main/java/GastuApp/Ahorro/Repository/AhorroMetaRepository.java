c39825e0efd295dbe005490ac32cb0adac425a1b
valeriavcgr <valeria3025gon@gmail.com>
2025-12-04 08:33:54 -0500

    [FIX] metodo ahorro meta repository

--- src/main/java/GastuApp/Ahorro/Repository/AhorroMetaRepository.java
+++ src/main/java/GastuApp/Ahorro/Repository/AhorroMetaRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import GastuApp.Ahorro.Entity.AhorroMeta;
 import GastuApp.Ahorro.Entity.AhorroMeta.Estado;
+import GastuApp.Conceptos.Entity.Concepto;
 
 @Repository
 public interface AhorroMetaRepository extends JpaRepository<AhorroMeta, Long>{
@@ -16,7 +17,7 @@ public interface AhorroMetaRepository extends JpaRepository<AhorroMeta, Long>{
     List<AhorroMeta> findByUsuarioIdOrderByFechaCreacionDesc(Long usuarioId);
 
 // Buscar un ahorro por id asegurando que pertenece al usuario
-    Optional<AhorroMeta> findByAhorroMetaIdAndUsuarioId(Long ahorroId, Long usuarioId);
+    Optional<AhorroMeta> findByAhorroIdAndUsuarioId(Long ahorroId, Long usuarioId);
 
 // filtra por estado
     List<AhorroMeta> findByUsuarioIdAndEstadoOrderByFechaCreacionDesc(
@@ -31,14 +32,8 @@ public interface AhorroMetaRepository extends JpaRepository<AhorroMeta, Long>{
     );
 
  // busca por el nombre parcial del concepto
-@Query("SELECT a FROM AhorroMeta a " +
-       "JOIN Concepto c ON a.conceptoId = c.conceptoId " +
-       "WHERE a.usuarioId = :usuarioId " +
-       "AND LOWER(c.nombre) LIKE LOWER(CONCAT('%', :texto, '%')) " +
-       "ORDER BY a.fechaCreacion DESC")
-List<AhorroMeta> buscarPorConceptoParcial(
-        @Param("usuarioId") Long usuarioId,
-        @Param("texto") String texto
-);
+@Query("SELECT a FROM AhorroMeta a, Concepto c WHERE a.conceptoId = c.id AND a.usuarioId = :usuarioId AND LOWER(c.nombre)" +
+       "LIKE LOWER(CONCAT('%', :texto, '%')) ORDER BY a.fechaCreacion DESC")
+    List<AhorroMeta> buscarPorConceptoParcial(@Param("usuarioId") Long usuarioId, @Param("texto") String texto);
     
 }
